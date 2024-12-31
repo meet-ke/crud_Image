cd796e0f3d66ee7affb419336c191d6e2624c59b
meet-ke <meet.darji@kernelequity.com>
2024-12-31 14:28:27 +0530

    Fixed File

--- crud.php
+++ crud.php
@@ -30,7 +30,9 @@
         foreach($_POST as $key => $value)
         {
             $_POST[$key] = mysqli_real_escape_string($con,$value);
-            $imgpath = image_uplode($_FILES['image']);    
+        }
+            $imgpath = image_uplode($_FILES['image']);   
+        
 
             $query = "INSERT INTO `product`(`name`, `price`, `description`, `image`) 
             VALUES ('$_POST[name]','$$_POST[price]','$$$_POST[desc]','$imgpath') ";
@@ -43,7 +45,7 @@
                 header("Location:index.php?alert=add_filed");
             }
         }
-    }
+    
 
     if(isset($_GET['rem']) && $_GET['rem']>0)
     {
@@ -53,7 +55,7 @@
 
         image_remove($fetch['image']);
 
-        $query="DELETE FROM `product` WHERE id='$_GET[rem]'";
+        $query="DELETE FROM `product` WHERE id=$_GET[rem]";
         if(mysqli_query( $con,$query)){
             header("Location:index.php?success=removed");
         }
@@ -62,4 +64,36 @@
             header("Location:index.php?alert =removed_failed");
         }
     }
+
+    if(isset($_POST['editproduct']))
+    {
+        foreach($_POST as $key => $value)
+        {
+            $_POST[$key] = mysqli_real_escape_string($con,$value);
+        }
+        
+        if(file_exists($_FILES['iamge']['tmp_name']) || is_uploaded_file($_FILES['iamge']['tmp_name']))
+        {
+            $query = "SELECT * FROM `product` WHERE id='$_POST[editpid]'";
+            $result=mysqli_query($con,$query);
+            $fetch=mysqli_fetch_array($result);  
+
+            image_remove($fetch['image']);
+
+            image_uplode($_FILES['image']);
+
+            $update="UPDATE `product` SET `name`='$_POST[name]',`price`='$_POST[price]',
+            `description`='$_POST[desc]',`image`='$imagepath ' WHERE 1";
+        }
+        else{
+            $update="UPDATE `product` SET `name`='$_POST[name]',`price`='$_POST[price]',
+            `description`='$_POST[desc]' WHERE 1";
+        }
+        if(mysqli_query($con,$update)){
+            header("loacation:index.php?success=updated");
+        }    
+        else{
+            header("location:index.php?alert=update_failed");
+        }
+    }  
 ?>
--- index.php
+++ index.php
@@ -15,6 +15,8 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    
 </head>
 <body class="bg-light">
     <div class="container bg-dark text-light p-3 rounded my-4">
@@ -60,7 +62,7 @@
                                 <td>$fetch[price]</td>
                                 <td>$fetch[description]</td>
                                 <td>
-                                    <a>Edit</a>
+                                    <a href="?edit=$fetch[id]" class="btn btn-warning me-2"><i class="bi bi-pencil-square"></i></a>
                                     <button onclick="confirm_rem($fetch[id])" class="btn btn-danger"><i class="bi bi-trash-fill"></i></button>
                                 </td>
 
@@ -79,7 +81,6 @@
                 <div class="modal-content">
                     <div class="modal-header">
                     <h5 class="modal-title">Add Product</h5>
-                   
                 </div>
                 <div class="modal-body">
                     <div class="input-group mb-3">
@@ -111,6 +112,69 @@
         </div>
     </div> 
 
+    <div class="modal fade" id="editproduct" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <form action="crud.php" method="POST" enctype="multipart/form-data">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <h5 class="modal-title">Edit Product</h5>
+                </div>
+                <div class="modal-body">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Name</span>
+                        <input type="text" class="form-control" name="name" id="editname" required>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Price</span>
+                        <input type="number" class="form-control" name="price" id="editprice" min="1" required>
+                    </div>
+               
+                    <div class="input-group">
+                        <span class="input-group-text">Description</span>
+                        <textarea class="form-control" name="desc" id="editdesc" required></textarea>
+                    </div>
+
+                    <img src="" id="editimage" width="100px" class="mb-3"><br>
+
+                    <div class="input-group mb-3">
+                        <input type="file" class="form-control" name="image" accept=".jpeg,.png,.svg">
+                        <label class="input-group-text" >Image</label>
+                    </div>
+                    <input type="hidden" name="productid" id="editpid">
+                </div>
+                <div class="modal-footer">
+                    <button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-success" name="editproduct">Edit</button>
+                </div>
+                </div>
+            </form>
+        </div>
+    </div> 
+
+    <?php
+        
+        if(isset($_GET['edit']) && $_GET['edit']>0)
+        {
+            $query = "SELECT * FROM `product` WHERE id='$_GET[edit]'";
+            $result = mysqli_query($con, $query);
+            $fetch = mysqli_fetch_assoc($result);
+            echo "
+            <script>
+                var myModal = new bootstrap.Modal(document.getElementById('editproduct'),{
+                keyboard: true
+                });
+                document.querySelector('#editname').value=$fetch[name];
+                document.querySelector('#editprice').value=$fetch[price];
+                document.querySelector('#editdesc').value=$fetch[description];
+                document.querySelector('#editimg').src=$fetch_src$fetch[image];
+                document.querySelectory('#editpid').value=$fetch[id];
+                editproduct.show();
+            </script>
+            ";
+        }
+    ?>
+
     <script>
         function confirm_rem(id){
             if(confirm("Are You Sure, you Want to delete this item ?")){
@@ -118,6 +182,5 @@
             }
         }
     </script>
-
 </body>
 </html>
